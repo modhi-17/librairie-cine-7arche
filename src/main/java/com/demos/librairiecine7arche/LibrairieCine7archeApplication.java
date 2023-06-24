88655dce06ba83305aa8fc8b02aa7b283449a0e0
Mohamed Dhif <mohamed.dhif@octo.com>
2023-06-24 15:49:46 +0200

    Initial commit

---
+++ src/main/java/com/demos/librairiecine7arche/LibrairieCine7archeApplication.java
@@ -0,0 +1,13 @@
+package com.demos.librairiecine7arche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrairieCine7archeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrairieCine7archeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demos/librairiecine7arche/LibrairieCine7archeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demos.librairiecine7arche;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrairieCine7archeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
