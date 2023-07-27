0ecffe00211061a9719ef469f38938c97cbecd68
Mohamed Dhif <mohamed.dhif@octo.com>
2023-07-27 10:01:03 +0200

    ajout des repository

---
+++ src/main/java/com/demos/librairiecine7arche/repository/EditeurRepository.java
@@ -0,0 +1,7 @@
+package com.demos.librairiecine7arche.repository;
+
+import com.demos.librairiecine7arche.model.Editeur;
+import org.springframework.data.repository.CrudRepository;
+
+public interface EditeurRepository extends CrudRepository<Editeur, Long> {
+}
